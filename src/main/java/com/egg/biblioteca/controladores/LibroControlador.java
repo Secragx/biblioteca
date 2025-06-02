2406de63ef72dc120f4abeb9accb55be4125e673
secragx <jesusdavidgarcesdiaz@gmail.com>
2025-06-02 17:36:31 -0500

    MODULOOS MODIFICAR IMPLEMENTADOS

--- src/main/java/com/egg/biblioteca/controladores/LibroControlador.java
+++ src/main/java/com/egg/biblioteca/controladores/LibroControlador.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -66,10 +67,42 @@ public class LibroControlador {
     }
 
     @GetMapping("/lista")
-    public String listar (ModelMap modelo){
-        List <Libro> libros = libroServicio.listarLibros();
+    public String listar(ModelMap modelo) {
+        List<Libro> libros = libroServicio.listarLibros();
         modelo.addAttribute("libros", libros);
 
         return "libro_list.html";
+    }
+
+    @GetMapping("/modificar/{isbn}")
+    public String modificar(@PathVariable Long isbn, ModelMap modelo) {
+        System.out.print("lso datos del libro son"+isbn);
+        Libro libro = libroServicio.getOne(isbn);
+        modelo.put("libro", libro);
+        modelo.addAttribute("autores", autorServicio.listarAutores());
+        modelo.addAttribute("editoriales", editorialServicio.listarEditoriales());
+        modelo.addAttribute("autorSeleccionado", libro.getAutor().getId());
+        modelo.addAttribute("editorialSeleccionada", libro.getEditorial().getId());
+        return "libro_modificar.html";
+    }
+
+    @PostMapping("/modificar/{isbn}")
+    public String modificar(@PathVariable @RequestParam(required = false) Long isbn, @RequestParam String titulo,
+            @RequestParam(required = false) Integer ejemplares, @RequestParam String idAutor,
+            @RequestParam String idEditorial, ModelMap modelo) {
+        try {
+            libroServicio.modificarLibro(isbn, titulo, ejemplares, idAutor, idEditorial);
+
+            return "redirect:../lista";
+        } catch (MiException ex) {
+            modelo.put("error", ex.getMessage());
+
+              List<Autor> autores = autorServicio.listarAutores();
+            List<Editorial> editoriales = editorialServicio.listarEditoriales();
+
+            modelo.addAttribute("autores", autores);
+            modelo.addAttribute("editoriales", editoriales);
+            return "libro_modificar.html";
+        }
     }
 }
--- src/main/java/com/egg/biblioteca/servicios/LibroServicio.java
+++ src/main/java/com/egg/biblioteca/servicios/LibroServicio.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.egg.biblioteca.entidades.Autor;
 import com.egg.biblioteca.entidades.Editorial;
@@ -16,7 +17,7 @@ import com.egg.biblioteca.repositorios.AutorRepositorio;
 import com.egg.biblioteca.repositorios.EditorialRepositorio;
 import com.egg.biblioteca.repositorios.LibroRepositorio;
 
-import jakarta.transaction.Transactional;
+
 
 @Service
 public class LibroServicio {
@@ -29,6 +30,11 @@ public class LibroServicio {
 
     @Autowired
     private EditorialRepositorio editorialRepositorio;
+
+    @Transactional(readOnly = true)
+    public Libro getOne(Long isbn) {
+        return libroRepositorio.getReferenceById(isbn);
+    }
 
     @Transactional
     public void crearLibro(Long isbn, String titulo, Integer ejemplares, String idAutor, String idEditorial) throws MiException{
@@ -58,7 +64,7 @@ public class LibroServicio {
         return libros;
     }
 
-    public void modificarLibro(Long isbn, String titulo, String idAutor, String idEditorial, Integer ejemplares) throws MiException{
+    public void modificarLibro(Long isbn, String titulo, Integer ejemplares, String idAutor, String idEditorial) throws MiException{
         
         validar(isbn, titulo, ejemplares, titulo, idEditorial);
 
@@ -87,6 +93,7 @@ public class LibroServicio {
         libroRepositorio.save(libro);
         }
     }
+
     private void validar(Long isbn, String titulo, Integer ejemplares, String idAutor, String idEditorial) throws MiException{
 if(isbn==null){
             throw new MiException ("EL isbn NO PUEDE SER NULO");
